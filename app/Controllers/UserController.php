5bc714f4da4f58c69ded0660ce2b8c4420bf14a5
Rogerio Leal <rogerioleal1@gmail.com>
2022-03-19 11:17:19 -0300

    (feature) WIP - users module

---
+++ app/Controllers/UserController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class UserController extends BaseController
+{
+    public function index()
+    {
+        return $this->response->setJSON([
+            'name' => 'User Test',
+        ]);
+    }
+}
